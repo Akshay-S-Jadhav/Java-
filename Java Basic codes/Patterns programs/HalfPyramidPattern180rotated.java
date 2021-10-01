b4226ae6d3d3ca2435abf41ccc41c38df1606f0e
Akshay Sandip Jadhav <90172410+Akshay-S-Jadhav@users.noreply.github.com>
2021-10-01 23:05:23 +0530

    Add files via upload

---
+++ Java Basic codes/Patterns programs/HalfPyramidPattern180rotated.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HalfPyramidPattern180rotated {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows of pyramid : ");
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<i; j++) {
+                System.out.print(" ");
+            }
+  
+            for(int j=0; j<=n-i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
